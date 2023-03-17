946d00571639aeb3c676bdc18c1a67aa3edd0784
ramelitomartinez <ramelitomartinez@gmail.com>
2023-03-17 15:27:53 -0400

    feat: added hit counter

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,9 @@
+import RetroHitCounter from 'react-retro-hit-counter';
 const Footer=()=>{
-    let copyrightYear=(/\d{4}/.exec(Date())[0])
+    const copyrightYear=(/\d{4}/.exec(Date())[0])
+    
+    //page counter reference
+    //https://www.npmjs.com/package/react-retro-hit-counter
     return (
         <footer>
             <a target='_blank'
@@ -14,6 +18,22 @@ const Footer=()=>{
                 </a>
             </p>
             <p>Copyright &copy; All Rights Reserved {copyrightYear}</p>
+            <RetroHitCounter
+            hits={0}
+           withBorder={true}
+           withGlow={false}
+           minLength={4}
+           size={40}
+           padding={4}
+           digitSpacing={3}
+           segmentThickness={4}
+           segmentSpacing={0.5}
+           segmentActiveColor="#76FF03"
+           segmentInactiveColor="#315324"
+           backgroundColor="#222222"
+           borderThickness={7}
+           glowStrength={0.5}
+            />
         </footer>
     )
 }
